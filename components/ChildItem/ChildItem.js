4dbdc2e12cb7a0f99df1d4dbcffd56c730a5b92b
DogwishX <zefiru@protonmail.com>
2022-06-28 22:52:46 +0100

    Add sign in/sign out functionality `Relates #5`

--- components/ChildItem/ChildItem.js
+++ components/ChildItem/ChildItem.js
@@ -1,7 +1,11 @@
 import { StyleSheet, Text, View, Button, Image } from "react-native";
+import { useEffect } from "react/cjs/react.production.min";
+import useToggle from "../../hooks/useToggle";
 
 export default function ChildItem({ childDetails }) {
   const { name, profilePic, signedInAt } = childDetails;
+  const [isSignedIn, toggleIsSignedIn] = useToggle(Boolean(signedInAt));
+
   const timeString =
     signedInAt && `${signedInAt.getHours()}:${signedInAt.getMinutes()}`;
 
@@ -10,8 +14,21 @@ export default function ChildItem({ childDetails }) {
       <Text>{name}</Text>
       <Image testID="childImg" source={profilePic}></Image>
       <Text testID="childStatus">
-        {!signedInAt ? "Signed Out" : `Signed in at ${timeString}`}
+        {isSignedIn ? `Signed in at ${timeString}` : "Signed Out"}
       </Text>
+
+      <Button
+        testID="signInOutBtn"
+        title={isSignedIn ? "Sign Out" : "Sign In"}
+        onPress={handlePress}
+      />
     </View>
   );
+
+  function handlePress() {
+    if (isSignedIn) return toggleIsSignedIn();
+
+    toggleIsSignedIn();
+    childDetails.signedInAt = new Date(Date.now());
+  }
 }
--- components/ChildItem/ChildItem.test.js
+++ components/ChildItem/ChildItem.test.js
@@ -1,21 +1,26 @@
 import ChildItem from "./ChildItem";
-import { render } from "@testing-library/react-native";
+import { render, fireEvent } from "@testing-library/react-native";
 
 function TestComponent(details) {
-  const { getByText, getByTestId } = render(
+  const { getByText, getByTestId, getByRole } = render(
     <ChildItem
-      childDetails={details || { name: "Danny", image: "@assets/chibi.jpg" }}
+      childDetails={
+        details || {
+          name: "Danny",
+          profilePic: render("../../assets/chibi.jpg"),
+        }
+      }
     />
   );
 
-  return { getByText, getByTestId };
+  return { getByText, getByTestId, getByRole };
 }
 
 describe("Render elements", () => {
   it("displays the child's name", () => {
     const { getByText } = TestComponent();
-    const name = getByText("Danny").props.children;
-    expect(name).toBe("Danny");
+    const nameText = getByText("Danny").props.children;
+    expect(nameText).toBe("Danny");
   });
 
   it("creates the child's picture element", () => {
@@ -26,19 +31,68 @@ describe("Render elements", () => {
 
   it("displays signed out status", () => {
     const { getByTestId } = TestComponent();
-    const status = getByTestId("childStatus").props.children;
-    expect(status).toBe("Signed Out");
+    const statusText = getByTestId("childStatus").props.children;
+    expect(statusText).toBe("Signed Out");
   });
 
   it("displays signed in status", () => {
     const now = new Date(Date.now());
     const { getByTestId } = TestComponent({
       name: "Danny",
-      image: "@assets/chibi.jpg",
+      profilePic: render("../../assets/chibi.jpg"),
       signedInAt: now,
     });
 
-    const status = getByTestId("childStatus").props.children;
-    expect(status).toBe(`Signed in at ${now.getHours()}:${now.getMinutes()}`);
+    const statusText = getByTestId("childStatus").props.children;
+    expect(statusText).toBe(
+      `Signed in at ${now.getHours()}:${now.getMinutes()}`
+    );
+  });
+
+  it("displays sign in button", () => {
+    const { getByText } = TestComponent();
+    const button = getByText("Sign In");
+    expect(button).toBeTruthy();
+  });
+
+  it("displays sign out button", () => {
+    const { getByText } = TestComponent({
+      name: "Danny",
+      profilePic: render("../../assets/chibi.jpg"),
+      signedInAt: new Date(Date.now()),
+    });
+    const button = getByText("Sign Out");
+
+    expect(button).toBeTruthy();
+  });
+});
+
+describe("When user taps button", () => {
+  test("displays 'Signed in at ...' when signed in", () => {
+    const { getByTestId } = TestComponent();
+
+    const button = getByTestId("signInOutBtn");
+    fireEvent.press(button);
+
+    const now = new Date(Date.now());
+
+    const statusText = getByTestId("childStatus").props.children;
+    expect(statusText).toBe(
+      `Signed in at ${now.getHours()}:${now.getMinutes()}`
+    );
+  });
+
+  test("displays 'Signed out' when signed out", () => {
+    const { getByTestId } = TestComponent({
+      name: "Danny",
+      profilePic: render("../../assets/chibi.jpg"),
+      signedInAt: new Date(Date.now()),
+    });
+
+    const button = getByTestId("signInOutBtn");
+    fireEvent.press(button);
+
+    const statusText = getByTestId("childStatus").props.children;
+    expect(statusText).toBe("Signed Out");
   });
 });
